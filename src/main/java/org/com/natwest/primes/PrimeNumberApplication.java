d4f563c0b892fcc28f0daf549589397722dc2b30
tyagi <tyagi.ankush94@gmail.com>
2023-03-26 16:34:01 +0100

    First commit for get primes

---
+++ src/main/java/org/com/natwest/primes/PrimeNumberApplication.java
@@ -0,0 +1,13 @@
+package org.com.natwest.primes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeNumberApplication.class, args);
+    }
+
+}
